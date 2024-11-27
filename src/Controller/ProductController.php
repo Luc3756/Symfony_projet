ec69bb99be2e1f10510e6ffbb72b08a7781dbf8d
Luc3756 <mluc020203@gmail.com>
2024-11-27 23:18:49 +0100

    recherche dynamique

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,9 +1,10 @@
 <?php
+
 namespace App\Controller;
 
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
-use Knp\Component\Pager\PaginatorInterface; // Importer le paginator
+use Knp\Component\Pager\PaginatorInterface; 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,14 +15,43 @@ class ProductController extends AbstractController
     #[Route('/products', name: 'product_list')]
     public function list(Request $request, EntityManagerInterface $entityManager, PaginatorInterface $paginator): Response
     {
-        
         $queryBuilder = $entityManager->getRepository(Product::class)->createQueryBuilder('p');
+        
+        $pagination = $paginator->paginate(
+            $queryBuilder,        
+            $request->query->getInt('page', 1), 
+            10                       
+        );
+
+        return $this->render('product/index.html.twig', [
+            'pagination' => $pagination, 
+        ]);
+    }
+
+    #[Route('/products/search', name: 'product_search', methods: ['GET'])]
+    public function search(Request $request, EntityManagerInterface $entityManager, PaginatorInterface $paginator): Response
+    {
+        $searchQuery = $request->query->get('q', '');
+
+        $queryBuilder = $entityManager->getRepository(Product::class)->createQueryBuilder('p');
+
+        if ($searchQuery) {
+            $queryBuilder
+                ->where('p.name LIKE :searchQuery')
+                ->setParameter('searchQuery', '%' . $searchQuery . '%');
+        }
 
         $pagination = $paginator->paginate(
             $queryBuilder,        
             $request->query->getInt('page', 1), 
-            10                      
+            10                       
         );
+
+        if ($request->isXmlHttpRequest()) {
+            return $this->render('product/_product_list.html.twig', [
+                'pagination' => $pagination, 
+            ]);
+        }
 
         return $this->render('product/index.html.twig', [
             'pagination' => $pagination, 
@@ -42,4 +72,3 @@ class ProductController extends AbstractController
         ]);
     }
 }
-
