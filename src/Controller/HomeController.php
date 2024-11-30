bed0ac7d3eceb02e386ce08f52ddd72ef3aa54a3
Luc3756 <mluc020203@gmail.com>
2024-11-30 22:03:21 +0100

    redirectionHome

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,11 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
+    #[Route('/', name: 'home_redirect')]
+    public function redirectToHome(): RedirectResponse
+    {
+        return new RedirectResponse($this->generateUrl('home'));
+    }
+    
     #[Route('/home', name: 'home')]
     public function index(): Response
     {
